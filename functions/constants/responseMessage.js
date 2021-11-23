eb5d571a567ee47cf8991ce2816cf676c4a79821
seol <suljiwon@naver.com>
2021-11-23 23:04:40 +0900

    feat: add jwt, responseMessage, statusCode to constanst

---
+++ functions/constants/responseMessage.js
@@ -0,0 +1,27 @@
+module.exports = {
+  NULL_VALUE: '필요한 값이 없습니다',
+  OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
+
+  // 포스트
+  ADD_ONE_POST_SUCCESS: '포스트 추가 성공',
+  READ_ONE_POST_SUCCESS: '포스트 조회 성공',
+  READ_ALL_POSTS_SUCCESS: '모든 포스트 조회 성공',
+  UPDATE_ONE_POST_SUCCESS: '포스트 수정 성공',
+  DELETE_ONE_POST_SUCCESS: '포스트 삭제 성공',
+  NO_POST: '존재하지 않는 포스트입니다.',
+
+
+  // 코멘트
+  ADD_ONE_COMMENT_SUCCESS: '한 마디 추가 성공',
+  READ_ONE_COMMENT_SUCCESS: '한 마디 조회 성공',
+  READ_ALL_COMMENTS_SUCCESS: '모든 한 마디 조회 성공',
+  NO_COMMENT: '존재하지 않는 한 마디입니다.',
+
+  // 서버 내 오류
+  INTERNAL_SERVER_ERROR: '서버 내 오류',
+
+  // 토큰
+  TOKEN_EXPIRED: '토큰이 만료되었습니다.',
+  TOKEN_INVALID: '토큰이 유효하지 않습니다.',
+  TOKEN_EMPTY: '토큰이 없습니다.',
+};

6627f3d5dbdc6767e9988e8b7aa400dc5a817bc3
seol <suljiwon@naver.com>
2021-11-29 02:33:03 +0900

    feat: add responseMessage for user

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -11,7 +11,9 @@ module.exports = {
   LOGIN_SUCCESS: '로그인 성공',
   LOGIN_FAIL: '로그인 실패',
   NO_USER: '존재하지 않는 회원입니다.',
+  MISS_MATCH_USER: '유저가 맞지 않습니다.',
   MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
+  INVALID_EMAIL: '이메일 형식을 확인해주세요.',
 
   // 프로필 조회
   READ_PROFILE_SUCCESS: '프로필 조회 성공',
@@ -37,4 +39,7 @@ module.exports = {
   TOKEN_EXPIRED: '토큰이 만료되었습니다.',
   TOKEN_INVALID: '토큰이 유효하지 않습니다.',
   TOKEN_EMPTY: '토큰이 없습니다.',
+
+  // 인증
+  NO_AUTH_HEADER: 'Authorization 헤더가 없습니다.',
 };

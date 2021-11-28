3416b2ed85176646ea78db4388b7f6e37b729f27
seol <suljiwon@naver.com>
2021-11-28 23:19:54 +0900

    fix: fix order of index routes

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/user', require('./user'));
 router.use('/auth', require('./auth'));
+router.use('/user', require('./user'));
 router.use('/post', require('./post'));
 
 module.exports = router;

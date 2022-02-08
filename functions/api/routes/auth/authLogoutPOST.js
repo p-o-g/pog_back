d8ec12e6a89b2772edfeb5c448dde7a1cb7d284c
seol <suljiwon@naver.com>
2022-02-08 21:36:35 +0900

    feat: add logout

---
+++ functions/api/routes/auth/authLogoutPOST.js
@@ -0,0 +1,26 @@
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { userDB } = require('../../../db');
+
+module.exports = async (req, res) => {
+  let client;
+
+  try {
+    client = await db.connect(req);
+
+    const user = await userDB.getUserById(req.user.id);
+    if (!user) {
+      res.status(statusCode.NOT_FOUND).json(util.fail(statusCode.NOT_FOUND, responseMessage.NO_USER));
+    }
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.LOGOUT_SUCCESS, user));
+  } catch (error) {
+    console.log(error);
+
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { checkUser } = require('../../../middlewares/auth');
 
 router.post('/signup', require('./authSignupPOST'));
 router.post('/login/email', require('./authLoginEmailPOST'));
+router.post('/logout', checkUser, require('./authLogoutPOST'));
 
 module.exports = router;

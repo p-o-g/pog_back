7e7ea162f6cc2385482d647ec1b56729470fda76
seol <suljiwon@naver.com>
2022-02-25 21:52:45 +0900

    [FEAT] post list search refactor, search tag name possible

--- functions/api/routes/post/index.js
+++ functions/api/routes/post/index.js
@@ -4,12 +4,11 @@ const router = express.Router();
 
 const uploadImage = require('../../../middlewares/uploadImage');
 
-router.get('/list', require('./postListGET'));
 router.post('/', uploadImage, require('./postPOST'));
-router.post('/tagSearch', require('./postTagSearchPOST'));
-router.get('/search', require('./postSearchGET'));
+router.get('/list', require('./postListGET'));
 router.get('/:postId', require('./postGET'));
 router.put('/:postId', checkUser, uploadImage, require('./postPUT'));
 router.delete('/:postId', checkUser, require('./postDELETE'));
+// router.post('/tagSearch', require('./postTagSearchPOST'));
 
 module.exports = router;
--- functions/api/routes/post/postListGET.js
+++ functions/api/routes/post/postListGET.js
@@ -4,27 +4,52 @@ const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
-const { postDB, relationPostTagDB, tagDB } = require('../../../db');
+const { postDB, relationPostTagDB, tagDB, userDB } = require('../../../db');
 
 module.exports = async (req, res) => {
+  const { search } = req.query;
+
   let client;
 
   try {
     client = await db.connect(req);
 
-    const posts = await postDB.getAllPosts(client);
-    const relationPostTags = await relationPostTagDB.getAllRelationPostTags(client);
-    const tags = await tagDB.getAllTags(client);
-
-    for (let i = 0; i < relationPostTags.length; i++) {
-      relationPostTags[i].tag = _.find(tags, (tag) => tag.id === relationPostTags[i].tagId);
+    const relationPostTagList = await relationPostTagDB.getRelationPostTagList(client);
+    let postList;
+    if (!search) {
+      postList = await postDB.getPostList(client);
+    } else {
+      postList = await postDB.getPostListBySearch(client, search);
     }
 
-    for (let i = 0; i < posts.length; i++) {
-      posts[i].tags = _.filter(relationPostTags, (pt) => pt.postId === posts[i].id).map((o) => o.tag);
-    }
+    postList = await Promise.all(
+      postList.map(async (post) => {
+        const writer = await userDB.getUserById(client, post.userId);
+        post.tagList = _.filter(relationPostTagList, (r) => r.postId === post.id).map((o) => {
+          return { id: o.tagId, name: o.tagName };
+        });
+        return {
+          id: post.id,
+          thumbnail: post.thumbnail,
+          title: post.title,
+          writer: {
+            id: writer.id,
+            nickname: writer.nickname,
+          },
+          summary: post.summary,
+          tagList: post.tagList,
+        };
+      }),
+    );
 
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_POSTS_SUCCESS, posts));
+    // 검색 결과 전체 포스트 개수 반환
+
+    const data = {
+      postListCount: postList.length,
+      postList,
+    };
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_POSTS_SUCCESS, data));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);

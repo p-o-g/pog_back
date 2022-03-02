4d24eaa5862a6b8ab64fe5c140863f73ebef82fb
seol <suljiwon@naver.com>
2022-03-02 23:41:51 +0900

    feat: add auth check routes

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { checkUser } = require('../../../middlewares/auth');
 
+router.post('/check/email', require('./authCheckEmailPOST'));
+router.post('/check/nickname', require('./authCheckNicknamePOST'));
+router.post('/check/phone', require('./authCheckPhonePOST'));
 router.post('/signup', require('./authSignupPOST'));
 router.post('/login/email', require('./authLoginEmailPOST'));
 router.post('/logout', checkUser, require('./authLogoutPOST'));

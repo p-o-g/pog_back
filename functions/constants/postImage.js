94359b85eb0bcef00a1755ebcd7f51f83651fa81
seol <suljiwon@naver.com>
2022-03-06 16:31:04 +0900

    feat: add default image to constant

---
+++ functions/constants/postImage.js
@@ -0,0 +1,3 @@
+module.exports = {
+  DEFAULT_IMAGE_URL: 'https://firebasestorage.googleapis.com/v0/b/p-o-g-cf552.appspot.com/o/pog_default_image.jpeg?alt=media&token=abd06649-95d8-4b0d-935a-cefeb4321771',
+};

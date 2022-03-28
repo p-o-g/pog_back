abc33fc3eac61a9e3c0e12d4f1d1c7d4b407a469
seol <suljiwon@naver.com>
2022-03-29 02:50:55 +0900

    feat: implement post weight create

--- functions/api/routes/post/index.js
+++ functions/api/routes/post/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
-const uploadImage = require('../../../middlewares/uploadImage');
+const uploadImage = require('../../../middlewares/uploadWeight');
+const uploadWeight = require('../../../middlewares/uploadWeight');
 
 router.post('/', checkUser, uploadImage, require('./postPOST'));
+router.post('/weight', checkUser, uploadWeight, require('./postWeightPOST'));
 router.get('/list', require('./postListGET'));
 router.get('/:postId', require('./postGET'));
 router.put('/:postId', checkUser, uploadImage, require('./postPUT'));
